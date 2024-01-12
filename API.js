e174ea69c7ec87e416f1f95856ad6215788420bd
Ognelot01 <113699332+Ognelot01@users.noreply.github.com>
2024-01-12 06:13:20 +0300

    Update API.js

--- API.js
+++ API.js
@@ -395,4 +395,44 @@ window.onload = async () => {
         event.target.querySelector('#option1').onchange = () => formUpd(event);
         event.target.querySelector('#option2').onchange = () => formUpd(event);
     });
+
 }
+
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map",  {
+            center: [55.76, 37.64],
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+            objectManager = new ymaps.ObjectManager({
+                // Чтобы метки начали кластеризоваться, выставляем опцию.
+                clusterize: true,
+                // ObjectManager принимает те же опции, что и кластеризатор.
+                gridSize: 32,
+                clusterDisableClickZoom: true
+        });
+
+        myGeoObject = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [55.8, 37.8]
+            },
+           
+        });
+        
+        $.ajax('./js/coordinate.json').done(function (response) {
+            let geoData = [];
+            for(i = 7; i < response.length; i++){
+                geoData.push(response[i].geoData.coordinates);
+            }
+            console.log(geoData)
+            myMap.geoObjects
+                .add(myGeoObject)   
+                .add(new ymaps.Placemark(geoData[1]));  
+                console.log(geoData[1]);XMLDocument
+        });
+    }
