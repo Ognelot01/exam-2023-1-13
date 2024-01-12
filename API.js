d1fb319d825bb042e27c89b0ad9ee7128844a38c
Ognelot01 <113699332+Ognelot01@users.noreply.github.com>
2024-01-12 06:22:37 +0300

    Update API.js

--- API.js
+++ API.js
@@ -403,7 +403,8 @@ ymaps.ready(init);
 function init() {
     var myMap = new ymaps.Map("map",  {
             center: [55.76, 37.64],
-            zoom: 10
+            zoom: 10,
+            controls: ["routePanelControl"]
         }, {
             searchControlProvider: 'yandex#search'
         }),
